87e556eb1cbde4c14e6ca1bd42c67d5f74e1276f
withinmukesh <mr75331753@gmail.com>
2026-06-14 14:35:16 +0530

    Add files via upload

---
+++ Day-12, Strings Slicing.py
@@ -0,0 +1,20 @@
+name="Mukesh"
+friend="Abhiraj"
+anotherfriend="Shadab"
+
+print(len(name))
+print(name[0:5])
+print(name[0:6])
+print(name[0:])
+print(name[:4])
+print(name[-3:-1])
+print(name[-3:len(name)-1])
+
+
+ab=''' I am MR. Mukesh Ambani
+I am from Rampur and I am genius and multi Talented'''
+print("Let use a for loop\n")# Backward Salace n gives space
+
+for character in ab:
+  print(character)
+
